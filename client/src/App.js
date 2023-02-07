0eea02f50862ee1465e4f491906a5eee2255945e
BazoukaJo <jonathanpratte@hotmail.com>
2023-02-07 18:56:34 -0500

    correct some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import './normal.css';
 import React, { useState, useEffect } from 'react'
 import { useSpeechSynthesis } from 'react-speech-kit';
-const WindowsSpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const mic = new WindowsSpeechRecognition();
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
 mic.continuous = true;
 mic.interimResults = true;
 mic.lang = 'en-US';
@@ -18,9 +18,6 @@ mic.lang = 'en-US';
 function App() {
   // MAX_TOKENS defined as integer with a value assigned by parsing the result of "4096" to an integer.
   const MAX_TOKENS = parseInt(4096);
-
-  // PLACE_HOLDER set to "Prompt".
-  const PLACE_HOLDER = "Prompt";
 
   // DEFAULT_QUERY set to text "Ask me anything...".
   const DEFAULT_QUERY = "Ask me anything...";
@@ -49,30 +46,36 @@ function App() {
   // temperature is set with state hook with 0.5 as the initial value
   const [temperature, setTemperature] = useState(Number(0.5));
 
-  // maxTokens is set with state hook with 1000 parsed to an integer as the initial value
+  // maxTokens is set with state hook with 100 parsed to an integer as the initial value
   const [maxTokens, setMaxTokens] = useState(parseInt(100));
 
   // declare 'n' with state hook with 1 as initial value
-  const [n , setN] = useState(Number(1));
+  const [n, setN] = useState(Number(1));
 
   // declare 'best_of' with state hook with 1 as initial value
-  const [best_of , setBest_of] = useState(Number(1));
+  const [best_of, setBest_of] = useState(Number(1));
 
-  // call getEngines() when component is mounted
+  // call getEngines() once when component is mounted
   useEffect(() => {getEngines();}, []);
 
   // declare isListening as false with state hook, toggle when handleListen() called
   const [isListening, setIsListening] = useState(false);
+
   // eslint-disable-next-line
-  useEffect(() => {handleListen();}, [isListening]);
+  useEffect(() => {handleIsListening();}, [isListening]);
 
   // declare isReading as false with state hook toggle isReading() when called
   const [isReading, setIsReading] = useState(false);
+
   // eslint-disable-next-line
   useEffect(() => {handleReading(chatLog[chatLog.length-1].message);}, [isReading]);
 
   // voice synthesis hook
   const {speak, voices} = useSpeechSynthesis();
+
+  const isBooted = false;
+  // eslint-disable-next-line
+  useEffect(() => {handleEventListerner();}, [isBooted]);
 
   //###################### Async Functions #######################
   /**
@@ -82,7 +85,8 @@ function App() {
   async function getEngines(){
     fetch("http://localhost:3080/models")
     .then(res => res.json())
-    .then(data => setModels(data.models))
+    .then(data => setModels(data.models));
+    //console.log("getEngine passed");
   }
 
   /**
@@ -99,11 +103,12 @@ function App() {
    * @param e - the event object
    */
   async function handleSubmit(){
+    //console.log("**handleSubmit**");
     let chatLogNew = [...chatLog];
     if (input !== "") {
-      chatLogNew.push({ user: "user", message: `${prefix + (prefix === "" ? "" : " ; ") + input + (suffix === "" ? "" : " ; ") + suffix}` });
+      chatLogNew.push({ user:"user", message:`${prefix + (prefix === "" ? "" : " ; ") + input + (suffix === "" ? "" : " ; ") + suffix}` });
     } else {
-      chatLogNew.push({ user: "user", message: PLACE_HOLDER });
+      chatLogNew.push({ user:"user", message:"Prompt"});
     }
     const messages = chatLogNew?.map((message) => message.message).join("\n");
     setChatLog(chatLogNew);
@@ -226,7 +231,8 @@ function App() {
   * It also sets an 'onstart' event listener which shows the recorder, and an 'onresult'
   * event listener which sets the input to the transcript of what was said. Finally, it sets an 'onerror' event listener which logs any errors that occur.
   */
-  const handleListen = () => {
+  const handleIsListening = (e) => {
+    //console.log("handleIsListening passed");
     if (isListening) {
       mic.start();
       mic.onend = () => {
@@ -265,6 +271,7 @@ function App() {
   * be spoken and the hideMute() function will be called.
   */
   const handleReading = (message) => {
+    //console.log("handleReading passed");
     if (isReading) {
       //console.log(message);
       speak({text:message, voice:voices[2]});
@@ -277,36 +284,28 @@ function App() {
     }
   }
 
-  /*
-  * This function handles the copy to clipboard by navigator copy.
-  */
-  const copyToClipboard = (message) => {
-    navigator.clipboard.writeText(message);
-  }
-
-  // This function handle key down and up to cycle through history
+  // This function will handle Enter, Home, End, Up and Down key press
   let timer;
-  let keyEventHandler = function(key) {
+  let keyEventHandler = function(event) {
     clearTimeout(timer);
     timer = setTimeout(function(){
-      if (key.keyCode === 38) {//up key
+      //console(event.keyCode);
+      if (event.keyCode === 38) {//up key
         cycleHistory(1);
-      } else if (key.keyCode === 40) {//down key
+      } else if (event.keyCode === 40) {//down key
         cycleHistory(-1);
-      } else if (key.keyCode === 36) {//end key
+      } else if (event.keyCode === 36) {//end key
         setIsListening(prevState => !prevState);
-      } else if (key.keyCode === 35) {//home key
+      } else if (event.keyCode === 35) {//home key
         setIsReading(prevState => !prevState);
       }
-      else if (key.keyCode === 13) {//enter key
-        key.preventDefault();
-        key.stopPropagation();
-        //console.log("enter key pressed");
-        handleSubmit();
-      }
-    },0.001);
+    },0.0001);
   };
-  document.addEventListener("keydown", keyEventHandler);
+
+  function handleEventListerner() {
+    //console.log('addEventListernerCalled called!');
+    document.addEventListener("keydown", keyEventHandler);
+  }
 
   // This function cycle through history
   function cycleHistory(index){
@@ -326,7 +325,7 @@ function App() {
   return (
     <div className="App">
       <aside className="sidemenu">
-          <div className="side-menu-button" onClick={clearChat}>
+          <div className="side-menu-button" onClick={(e) => {clearChat()}}>
             <span>+ </span>New Chat
           </div>
         <div className="models">
@@ -362,12 +361,12 @@ function App() {
           ))}
         </div>
         <div className="Chat-input-holder">
-          <form className="form1" >
-            <input className="chat-input-textarea" placeholder={PLACE_HOLDER} autoFocus rows="1" value={input} onChange={(e) => setInput(e.target.value)} />
-            <input className="chat-input-textarea-prefix" placeholder='Prefix' value={prefix} onChange={(e) => setPrefix(e.target.value)}/>
-            <input className="chat-input-textarea-suffix" placeholder='Suffix' value={suffix} onChange={(e) => setSuffix(e.target.value)}/>
-          </form>
-          <button className='send-button' type="button" title="Send Prompt to GPT" onClick={handleSubmit} tabIndex="-1" onFocus={focusTheTextArea}>
+          <div className="form1" onKeyDown={(e) => {e.keyCode === 13 && handleSubmit()}}>
+            <input type="text" className="chat-input-textarea" placeholder="Prompt" autoFocus rows="1" value={input} onChange={(e) => setInput(e.target.value)} />
+          </div>
+          <input className="chat-input-textarea-prefix" placeholder='Prefix' value={prefix} onChange={(e) => setPrefix(e.target.value)}/>
+          <input className="chat-input-textarea-suffix" placeholder='Suffix' value={suffix} onChange={(e) => setSuffix(e.target.value)}/>
+          <button className='send-button' type="button"  title="Send Prompt to GPT" onClick={(e)=>{handleSubmit()}} tabIndex="-1" onFocus={(e) => {focusTheTextArea()}}>
             <svg width="16" height="27" fill="currentColor" viewBox="0 0 16 16"><path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/></svg>
           </button>
           <button className='record-voice-button' type="button" title="Record Voice To Prompt - Shortcut : Home"onClick={() => setIsListening(prevState => !prevState)}>
@@ -376,12 +375,12 @@ function App() {
               <svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="black" fill="red" /></svg>
             </div>
           </button>
-          <button className="copy-button" title="Copy Input To Clipboard"onClick={() => copyToClipboard(document.getElementsByClassName("chat-input-textarea")[0].value)}>
+          <button className="copy-button" title="Copy Input To Clipboard" onClick={(e)=> {navigator.clipboard.writeText(document.getElementsByClassName("chat-input-textarea")[0].value)}}>
             <div>
-              <svg width="16" height="16" fill="currentColor" viewBox="-2 -4 20 20"><path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/></svg>
+            <svg width="18" height="18" fill="currentColor" viewBox="1 -1 16 17"><path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/></svg>
             </div>
           </button>
-          <button className='read-button' title="Answers Read By AI - Shorcut : End" type="button" onClick={() => setIsReading(prevState => !prevState)}>
+          <button className='read-button' title="Answers Read By AI - Shorcut : End" type="button" onClick={(e) => setIsReading(prevState => !prevState)}>
             <div className="unmute">
               <svg width="20" height="20" fill="currentColor" viewBox="1.5 -1 16 16"><path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/><path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/><path d="M10.025 8a4.486 4.486 0 0 1-1.318 3.182L8 10.475A3.489 3.489 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.486 4.486 0 0 1 10.025 8zM7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12V4zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11z"/></svg>
             </div>
@@ -404,7 +403,8 @@ function App() {
         <svg width="40" height="40" fill="none" ><path d="M37.532 16.87a9.963 9.963 0 0 0-.856-8.184 10.078 10.078 0 0 0-10.855-4.835A9.964 9.964 0 0 0 18.306.5a10.079 10.079 0 0 0-9.614 6.977 9.967 9.967 0 0 0-6.664 4.834 10.08 10.08 0 0 0 1.24 11.817 9.965 9.965 0 0 0 .856 8.185 10.079 10.079 0 0 0 10.855 4.835 9.965 9.965 0 0 0 7.516 3.35 10.078 10.078 0 0 0 9.617-6.981 9.967 9.967 0 0 0 6.663-4.834 10.079 10.079 0 0 0-1.243-11.813ZM22.498 37.886a7.474 7.474 0 0 1-4.799-1.735c.061-.033.168-.091.237-.134l7.964-4.6a1.294 1.294 0 0 0 .655-1.134V19.054l3.366 1.944a.12.12 0 0 1 .066.092v9.299a7.505 7.505 0 0 1-7.49 7.496ZM6.392 31.006a7.471 7.471 0 0 1-.894-5.023c.06.036.162.099.237.141l7.964 4.6a1.297 1.297 0 0 0 1.308 0l9.724-5.614v3.888a.12.12 0 0 1-.048.103l-8.051 4.649a7.504 7.504 0 0 1-10.24-2.744ZM4.297 13.62A7.469 7.469 0 0 1 8.2 10.333c0 .068-.004.19-.004.274v9.201a1.294 1.294 0 0 0 .654 1.132l9.723 5.614-3.366 1.944a.12.12 0 0 1-.114.01L7.04 23.856a7.504 7.504 0 0 1-2.743-10.237Zm27.658 6.437-9.724-5.615 3.367-1.943a.121.121 0 0 1 .113-.01l8.052 4.648a7.498 7.498 0 0 1-1.158 13.528v-9.476a1.293 1.293 0 0 0-.65-1.132Zm3.35-5.043c-.059-.037-.162-.099-.236-.141l-7.965-4.6a1.298 1.298 0 0 0-1.308 0l-9.723 5.614v-3.888a.12.12 0 0 1 .048-.103l8.05-4.645a7.497 7.497 0 0 1 11.135 7.763Zm-21.063 6.929-3.367-1.944a.12.12 0 0 1-.065-.092v-9.299a7.497 7.497 0 0 1 12.293-5.756 6.94 6.94 0 0 0-.236.134l-7.965 4.6a1.294 1.294 0 0 0-.654 1.132l-.006 11.225Zm1.829-3.943 4.33-2.501 4.332 2.5v5l-4.331 2.5-4.331-2.5V18Z" fill="currentColor"/></svg>
       </div>
     </div>
-  );}
+  );
+}
   //###################### END Return HTML ########################
 
 /**
@@ -428,9 +428,9 @@ const ChatMessage = ({message}) => {
           {message.user === "user" &&
             <span className='simple-message'>{message.message}</span>}
         </div>
-        <button className="copy-current-button" title="Copy To Clipboard" onClick={navigator.clipboard.writeText(message.message)}>
+        <button className="copy-current-button" title="Copy To Clipboard" onClick={(e)=> {navigator.clipboard.writeText(message.message)}}>
           <div>
-            <svg width="16" height="16" fill="currentColor" viewBox="-2 -4 20 20"><path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/></svg>
+            <svg width="18" height="18" fill="currentColor" viewBox="1 -1 16 17"><path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/></svg>
           </div>
         </button>
       </div>
